9b32ac0db6ace6744b7981520fcaa75788a480ae
ronal cucariano <ronalcucariano@gmail.com>
2024-02-20 02:28:55 -0500

    Clase_2

---
+++ Views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nemo sapiente fuga sunt itaque modi expedita numquam, error vitae eos, quos sequi voluptatibus doloremque distinctio odit quis at veritatis suscipit?</h1>
+</body>
+</html>
