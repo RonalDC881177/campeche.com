f3d6240abcc5072d75dce716281b56860ebea684
ronal cucariano <ronalcucariano@gmail.com>
2024-02-21 15:29:13 -0500

    se inicia conexion a base de datos

--- Config/Config.php
+++ Config/Config.php
@@ -1,3 +1,8 @@
 <?php
 const base_url = "http://localhost/campeche.com/";
+const host = "locahost";
+const user = "root";
+const pass = "";
+const db = "sistema";
+const charset = "charset=utf8";
 ?>
--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -7,9 +7,9 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Panel de administración</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="<?php echo base_url; ?>Assets/css/style.min.css" rel="stylesheet" />
         <link href="<?php echo base_url; ?>Assets/css/styles.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url; ?>Assets/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
@@ -676,12 +676,12 @@
                 </footer>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
+        <script src="<?php echo base_url; ?>Assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url; ?>Assets/js/scripts.js"></script>
+        <script src="<?php echo base_url; ?>Assets/js/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url; ?>Assets/demo/chart-area-demo.js"></script>
+        <script src="<?php echo base_url; ?>Assets/demo/chart-bar-demo.js"></script>
+        <script src="<?php echo base_url; ?>Assets/js/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url; ?>Assets/js/datatables-simple-demo.js"></script>
     </body>
 </html>
